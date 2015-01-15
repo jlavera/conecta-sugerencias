5cf4b0a2049dfa44f1606518a43cde9e90c936b2
JHL <jlavera92@gmail.com>
2015-01-14 21:43:14 -0300

    bleh

--- app/Global.scala
+++ app/Global.scala
@@ -7,17 +7,17 @@ object Global extends GlobalSettings {
   override def onStart(app: Application) {
     Logger.info("configuring Neo4j for " + System.getenv("GRAPHENEDB_URL"))
 
-    //val neo4jURLString = util.Properties.envOrElse("GRAPHENEDB_URL","http://localhost:7474")
-    val url:URL = new URL(System.getProperty("GRAPHENEDB_URL"))
+    val neo4jURLString = util.Properties.envOrElse("GRAPHENEDB_URL", System.getProperty("GRAPHENEDB_URL"))
+    val url: URL = new URL(neo4jURLString)
 
     // testing locally don't have user/pass
-    if(url.getUserInfo != null) {
-      val Array(user:String, pass:String) = url.getUserInfo.split(":")
+    if (url.getUserInfo != null) {
+      val Array(user: String, pass: String) = url.getUserInfo.split(":")
       //Neo4jREST.setServer(url.getHost, url.getPort, "/db/data/", user, pass)
       Neo4jREST.setServer(url.getHost, url.getPort, "/db/data/", user, pass)
     } else {
       Neo4jREST.setServer(url.getHost, url.getPort, "/db/data/")
     }
   }
-  
+
 }
